b1d06e3aa13f963076cac82a4ab017e6374dd276
landrumb <36489943+landrumb@users.noreply.github.com>
2020-06-10 11:54:30 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1GT6IGYNSoTTs9-AFquvcEwfDClRyhh8U'
+export_file_name = 'birds_v1.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['scarlet_tanager', 'baltimore_oriole', 'northern_cardinal', 'brown_headed_cowbird', 'red_winged_blackbird', 'house_finch', 'dark_eyed_junco', 'purple_finch']
 path = Path(__file__).parent
 
 app = Starlette()
